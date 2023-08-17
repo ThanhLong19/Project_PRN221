27038cd45395342376cc035cec38124d1e6f0a1c
DESKTOP-OGRUR0H\longb <longbh1911@gmail.com>
2023-08-18 01:34:08 +0700

    fix error

---
+++ ProjectPRN221/ProjectPRN221/Pages/Product/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectPRN221.Pages.Product
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
