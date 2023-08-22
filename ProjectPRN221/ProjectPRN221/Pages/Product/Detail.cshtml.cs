f232d354095b3a6bb62c9694d8ab7e0fb871a3a4
ThanhLong <longuras1901@gmail.com>
2023-08-22 22:02:25 +0700

    list product, details and filter, no pagination

--- ProjectPRN221/ProjectPRN221/Pages/Product/Detail.cshtml.cs
+++ ProjectPRN221/ProjectPRN221/Pages/Product/Detail.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ProjectPRN221.Models;
 
 namespace ProjectPRN221.Pages.Product
 {
     public class DetailModel : PageModel
     {
-        public void OnGet()
+        private readonly NorthwindContext context;
+        public Models.Product Product { get; set; }
+
+        public DetailModel(NorthwindContext context)
         {
+            this.context = context;
+        }
+        public async Task<IActionResult> OnGetAsync(int? pid)
+        {
+            bool productExists = await context.Products.AnyAsync(p => p.ProductId == pid);
+            if (productExists)
+            {
+                Product = await context.Products.Include(p => p.Category).Where(p => p.ProductId == pid).SingleOrDefaultAsync();
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Product/List");
+            }
         }
     }
 }
