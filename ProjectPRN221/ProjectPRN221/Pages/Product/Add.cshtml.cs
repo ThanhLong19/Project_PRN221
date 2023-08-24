311c7567a7124cc8516e6049e5cd0f3891f48c88
toanpv224 <toanpv224@gmail.com>
2023-08-24 20:45:58 +0700

    Update editor

--- ProjectPRN221/ProjectPRN221/Pages/Product/Add.cshtml.cs
+++ ProjectPRN221/ProjectPRN221/Pages/Product/Add.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ProjectPRN221.Models;
 
 namespace ProjectPRN221.Pages.Product
@@ -16,6 +17,26 @@ namespace ProjectPRN221.Pages.Product
         }
         public void OnGet()
         {
+            //load du lieu cua cities
+            ViewData["SuppliersId"] = new SelectList(db.Suppliers.ToList(), "SupplierId", "CompanyName");
+            ViewData["CategoriesId"] = new SelectList(db.Categories.ToList(), "CategoryId", "CategoryName");
+        }
+
+        public async Task<ActionResult> OnPostAsync()
+        {
+            //if (ModelState.IsValid)
+            //{
+
+            Console.WriteLine(product.Description);
+            //Save data into DB
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return RedirectToPage("Manage");
+
+
+            //}
+
+            //return Page();
         }
     }
 }
